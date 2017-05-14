92e857810b96d4dcb66548fcde7fa905bb89e261
Computer Science <cpsc@node34.fullerton.edu>
2017-05-14 04:15:10 -0700

    Gaussian Blur

--- sealCounter.py
+++ sealCounter.py
@@ -27,17 +27,17 @@ def preprocessImages():
 	start_time = time.time()
 	#imageFile = open('/home/cpsc/MachineLearning/ProcessedKaggle/', 'w+')
 	#print('yes', imageFile)
-	imgDest = '/home/cpsc/MachineLearning/ComputerVision/KaggleCode/ProcessedKaggle/'
+	imgDest = '/home/cpsc/MachineLearning/ComputerVision/KaggleCode/ProcessedKaggle/gauss'
 	for filename in os.listdir('/home/cpsc/MachineLearning/ComputerVision/KaggleCode/Train/'):
 		img = imread('Train/'+filename)
 		img = rgb2gray(img)
 		#print(np.shape(img))	
 		#edges = filters.sobel(img)
 		gaus = filters.gaussian(img)
-		sobel = filters.sobel_h(gaus)
+		#sobel = filters.sobel_h(gaus)
 		#hog_image = hog(img)#, orientations = 8, pixels_per_cell=(16,16), cells_per_block(1,1), visualise=True)
 		#scipy.misc.imsave(imgDest + 'edges'+ filename , edges)
-		scipy.misc.imsave(imgDest + 'sobel+gause'+ filename, sobel)
+		scipy.misc.imsave(imgDest + 'gause'+ filename, gaus)
 	print("--- %s seconds --- to preprocess" % (time.time()- start_time))
 
 def sealForrest():
@@ -84,19 +84,19 @@ def flatten(img):
 
 def sealCV():
 	train_data = pd.read_csv('train.csv')
-	train_imgs = sorted(glob.glob('ProcessedKaggle/*.jpg'))# key = lambda name: int(os.path.basename(name)[:-4]))
+	train_imgs = sorted(glob.glob('ProcessedKaggle/gaussgause*.jpg'))# key = lambda name: int(os.path.basename(name)[:-4]))
 	train_dot = sorted(glob.glob('Kaggle/TrainSmall2/TrainDotted/*.jpg'))# key = lambda name: int(os.path.baseename(name)[:-4]))
 	#submission = pd.read_csv('~/MachineLearning/KaggleCode/sample_submission.csv')
 	print(train_data.shape)
 	print('Number of Train Images: {:d}'.format(len(train_imgs)))
 	print('Number of Dotted-Train Images: {:d}'.format(len(train_dot)))
 	print(train_data.head(6))
-	idx = 1
+	idx = 2
 	image = cv2.cvtColor(cv2.imread(train_imgs[idx]),cv2.COLOR_BGR2RGB)
 	image_dot = cv2.cvtColor(cv2.imread(train_dot[idx]),cv2.COLOR_BGR2RGB)
 	img = image[1350:1900, 3000:3400]
 	img_dot = image_dot[1350:1900, 3000:3400]
-	diff = cv2.absdiff(img,img_dot) #get the difference of the cropped images
+	diff = cv2.absdiff(image_dot,image) #get the difference of the cropped images
 	gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
 	ret, th1 = cv2.threshold(gray,0,255,cv2.THRESH_BINARY|cv2.THRESH_OTSU) #threash hold at 0 to further differentiate the ground from the seals
 	cnts = cv2.findContours(th1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
